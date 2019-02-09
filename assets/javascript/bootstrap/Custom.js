25db59e2085fd1627b7bf11d5683e2cda402fc85
dharmendraecb <46588108+dharmendraecb@users.noreply.github.com>
2019-02-09 12:53:40 +0530

    version 53

--- assets/javascript/bootstrap/Custom.js
+++ assets/javascript/bootstrap/Custom.js
@@ -4,8 +4,8 @@ var btns = ul.getElementsByClassName("nav-item");
 for (var i = 0; i < btns.length; i++) {
     alert(i);
   btns[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("active");
-  current[0].className = current[0].className.replace(" active", "");
-  this.className += " active";
+  var current = document.getElementsByClassName("action");
+      current[0].className = current[0].className.replace(" action", "");
+      this.className += " action";
   });
 }
