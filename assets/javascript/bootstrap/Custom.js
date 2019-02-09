31f93e46427dc9327bd375f3b6732012f77736fb
dharmendraecb <46588108+dharmendraecb@users.noreply.github.com>
2019-02-09 12:52:17 +0530

    Update Custom.js

--- assets/javascript/bootstrap/Custom.js
+++ assets/javascript/bootstrap/Custom.js
@@ -1,7 +1,8 @@
 ﻿
-var header = document.getElementById("main");
-var btns = header.getElementsByClassName("nav-item");
+var ul = document.getElementById("main");
+var btns = ul.getElementsByClassName("nav-item");
 for (var i = 0; i < btns.length; i++) {
+    alert(i);
   btns[i].addEventListener("click", function() {
   var current = document.getElementsByClassName("active");
   current[0].className = current[0].className.replace(" active", "");
