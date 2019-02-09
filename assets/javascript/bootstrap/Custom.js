d6b635e6a7d1c89eaeff4d731e137b7df72968f7
dharmendraecb <46588108+dharmendraecb@users.noreply.github.com>
2019-02-09 16:04:55 +0530

    Update Custom.js

--- assets/javascript/bootstrap/Custom.js
+++ assets/javascript/bootstrap/Custom.js
@@ -2,17 +2,11 @@
 
 $(document).ready(function () {
     var url = window.location;
-    $('ul a[href="' + url + '"]').parent().addClass('action');
+    $('ul.linetonav li a[href="' + url + '"]').parent().addClass('action');
   //  $('li a[href="' + url + '"]').parent().addClass('action');
-    $('ul.linetonav a').filter(function () {
+    $('ul.linetonav li a').filter(function () {
         return this.href === url;
     }).parent().addClass('actioninline');
 
-     var url1 = window.location;
-    $('li.inner a[href="' + url + '"]').parent().addClass('actioninline');
-  //  $('li a[href="' + url + '"]').parent().addClass('action');
-    $('li.inner a').filter(function () {
-        return this.href === url1;
-    }).parent().addClass('action');
    
 });
