c4116dbc7f97ea4e29788a1bd657f7c34e20f49b
dharmendraecb <46588108+dharmendraecb@users.noreply.github.com>
2019-02-10 11:00:45 +0530

    version 68

---
+++ assets/javascript/bootstrap/Custom.js
@@ -0,0 +1,13 @@
+﻿
+
+$(document).ready(function () {
+    var url = window.location;
+    $('ul.linetonav a[href="' + url + '"]').parent().addClass('action');
+
+    $('ul.linetonav a').filter(function () {
+        return this.href === url;
+    }).parent().addClass('active').parent().parent().parent().addClass('show');
+
+   
+});
+
