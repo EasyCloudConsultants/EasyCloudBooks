c0488d26be2579f47952296597c86700b71c6e88
Dharmendra Patel <46588108+dharmendraecb@users.noreply.github.com>
2019-02-10 09:37:52 +0530

    Update Custom.js

--- assets/javascript/bootstrap/Custom.js
+++ assets/javascript/bootstrap/Custom.js
@@ -1,12 +1,11 @@
 ﻿
-
 $(document).ready(function () {
     var url = window.location;
-    $('ul.linetonav li a[href="' + url + '"]').parent().addClass('action');
-  //  $('li a[href="' + url + '"]').parent().addClass('action');
-    $('ul.linetonav li a').filter(function () {
+    $('ul.linetonav a[href="' + url + '"]').parent().addClass('action');
+
+    $('ul.linetonav a').filter(function () {
         return this.href === url;
-    }).parent().addClass('actioninline');
+    }).parent().addClass('active').parent().parent().addClass('active');
 
    
 });
