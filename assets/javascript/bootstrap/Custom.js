7ef0eecf844068f258890719fe8c45e4c8d2fb28
Mitul Panchal <mitulpanch@users.noreply.github.com>
2019-02-10 15:26:16 +0530

    Update Custom.js

--- assets/javascript/bootstrap/Custom.js
+++ assets/javascript/bootstrap/Custom.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
     var url = window.href;
     $('.main-menu li').find('a[href="'+url+'"]').parents('li').addClass('active');
-    console.log($('.main-menu li').find('a[href="'+url+'"]').parents('li'));
+    console.log(url);
+    console.log($('.main-menu li').find('a[href="'+url+'"]'));
     $.each($('.main-menu li').find('a[href="'+url+'"]').parents('li').find('ul'),function(e,element){
         $(element).addClass('show');
     console.log($(element));
