2ba64892efdd9bf8beed2fa762356cdaf206d698
dharmendraecb <46588108+dharmendraecb@users.noreply.github.com>
2019-02-11 10:12:08 +0530

    version 71

--- assets/javascript/bootstrap/Custom.js
+++ assets/javascript/bootstrap/Custom.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
     var url = location.href;
-    $('.main-menu li').find('a[href="'+url+'"]').parents('li').addClass('active');
-    $.each($('.main-menu li').find('a[href="'+url+'"]').parents('li').find('ul'),function(e,element){
+    $('.main-menu li').find('a[href="' + url + '"]').parents('li').addClass('active');
+    $.each($('.main-menu li').find('a[href="' + url + '"]').parents('li').find('ul'), function (e, element) {
         $(element).addClass('show');
     });
+    $('.main-menu a[href="' + url + '"]').parents().addClass('action');
 });
