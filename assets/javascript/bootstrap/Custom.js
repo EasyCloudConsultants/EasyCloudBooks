ad225c74d77386cdedfbebbe303bd8c592a9da65
dharmendraecb <46588108+dharmendraecb@users.noreply.github.com>
2019-02-11 15:15:05 +0530

    version 78

--- assets/javascript/bootstrap/Custom.js
+++ assets/javascript/bootstrap/Custom.js
@@ -4,5 +4,24 @@ $(document).ready(function () {
     $.each($('.main-menu li').find('a[href="' + url + '"]').parents('li').find('ul'), function (e, element) {
         $(element).addClass('show');
     });
- 
+
+    fix_height();
+
+    function fix_height() {
+       // var heightWithoutNavbar = $("body > .page-content").height() - 61;
+   // $(".sidebard-panel").css("min-height", heightWithoutNavbar + "px");
+
+        var navbarHeigh = $('#main-menu').height();
+        var wrapperHeigh = $('.bd-content').height();
+        if (navbarHeigh > wrapperHeigh) {
+            $('.bd-content').css("min-height", navbarHeigh + "px");
+        }
+
+        if (navbarHeigh < wrapperHeigh) {
+            $('.bd-content').css("min-height", $(window).height() + "px");
+        }
+
+    }
+
+  
 });
