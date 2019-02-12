6a4c4a58ef9c1963002c98922a0ae1c93fc5bb2c
dharmendraecb <46588108+dharmendraecb@users.noreply.github.com>
2019-02-12 11:47:09 +0530

    Update Custom.js

--- assets/javascript/bootstrap/Custom.js
+++ assets/javascript/bootstrap/Custom.js
@@ -1,14 +1,7 @@
 $(document).ready(function () {
   
-    var url = location.href;
-    $('.main-menu li').find('a[href="' + url + '"]').parents('li').addClass('active');
-    $.each($('.main-menu li').find('a[href="' + url + '"]').parents('li').find('ul'), function (e, element) {
-        $(element).addClass('show');
-       
-       
-    });
- 
     heightadd();
+
     $("#main-menu").click(function () {
 
         heightadd();
@@ -16,11 +9,11 @@ $(document).ready(function () {
        
     });
 
-    //$(window).bind("load resize scroll", function () {
+    $(window).bind("load resize hover scroll", function () {
 
-    //    heightadd();
+       heightadd();
            
-    //});
+   });
    
   
 });
@@ -29,10 +22,24 @@ function heightadd() {
 
     var heightview = "";
 
-    $.each($('.main-menu li a').parents('li').find('ul'), function (e, element) {
+    var url = location.href;
+    $('.main-menu li').find('a[href="' + url + '"]').parents('li').addClass('active');
 
-        heightview = $(element).height();
-        alert(heightview);
+    $.each($('.main-menu li a').parents('li').find('ul'), function (e, element) {
+        $(element).removeClass('show');
+    });
+
+    $.each($('.main-menu li').find('a').parents('li').find('ul'), function (e, element) {
+        $(element).click(function () {
+
+            heightview = $(this).height();
+            alert(heightview);
+        });
+
+    });
+
+    $.each($('.main-menu li a').parents('li').find('ul'), function (e, element) {
+        $(element).removeClass('show');
     });
     var navbarHeigh = $('#main-menu').height();
     var wrapperHeigh = $('#collapsibleNavbar').height();
