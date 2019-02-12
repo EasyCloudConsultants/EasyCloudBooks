5f9b0f51cf00ab16dbcad5733e934136a698c6f0
Dharmendra Patel <46588108+dharmendraecb@users.noreply.github.com>
2019-02-12 09:48:25 +0530

    Update Custom.js

--- assets/javascript/bootstrap/Custom.js
+++ assets/javascript/bootstrap/Custom.js
@@ -10,12 +10,11 @@ $(document).ready(function () {
       
         var navbarHeigh = $('#main-menu').height();
        var wrapperHeigh = $('#collapsibleNavbar').height();
-       var heightadd = wrapperHeigh + 165;
-        alert(wrapperHeigh);
-        alert(navbarHeigh);
+       var heightadd = wrapperHeigh + 150;
+      
         if ( wrapperHeigh > navbarHeigh ) {
             $('#main-menu').css("min-height", heightadd + "px");
-            alert(heightadd);
+           
         }
         if (navbarHeigh < wrapperHeigh) {
             $('#main-menu').css("min-height", $(window).height() + "px");
