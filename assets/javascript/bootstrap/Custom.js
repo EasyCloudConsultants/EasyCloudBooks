f543a7af877a9fa2d109df8bf57ee8a2248c5580
dharmendraecb <46588108+dharmendraecb@users.noreply.github.com>
2019-02-12 10:05:04 +0530

    version 83

--- assets/javascript/bootstrap/Custom.js
+++ assets/javascript/bootstrap/Custom.js
@@ -5,23 +5,32 @@ $(document).ready(function () {
         $(element).addClass('show');
     });
 
+    heightadd();
 
-   $("#main-menu").click(function () {
-      
-        var navbarHeigh = $('#main-menu').height();
-       var wrapperHeigh = $('#collapsibleNavbar').height();
-       var heightadd = wrapperHeigh + 150;
-      
-        if ( wrapperHeigh > navbarHeigh ) {
-            $('#main-menu').css("min-height", heightadd + "px");
-           
-        }
-        if (navbarHeigh < wrapperHeigh) {
-            $('#main-menu').css("min-height", $(window).height() + "px");
-
-        }
-
+    $(window).bind("load resize scroll", function () {
+       
+        heightadd;
+        alert("hello");
     });
-
   
 });
+
+
+
+function heightadd()
+{
+
+    var navbarHeigh = $('#main-menu').height();
+    var wrapperHeigh = $('#collapsibleNavbar').height();
+    var heightadd = wrapperHeigh + 150;
+
+    if (wrapperHeigh > navbarHeigh) {
+        $('#main-menu').css("min-height", heightadd + "px");
+
+    }
+    if (navbarHeigh < wrapperHeigh) {
+        $('#main-menu').css("min-height", $(window).height() + "px");
+
+    }
+
+}
