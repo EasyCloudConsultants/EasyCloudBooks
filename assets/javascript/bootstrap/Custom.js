659ba8dde1908e0a99a2c4d85a4881c1756fe22a
dharmendraecb <46588108+dharmendraecb@users.noreply.github.com>
2019-02-12 10:14:20 +0530

    Update Custom.js

--- assets/javascript/bootstrap/Custom.js
+++ assets/javascript/bootstrap/Custom.js
@@ -7,11 +7,13 @@ $(document).ready(function () {
 
     heightadd();
 
+
     $(window).bind("load resize scroll", function () {
-       
+
         heightadd();
-  
+
     });
+   
   
 });
 
@@ -21,11 +23,11 @@ function heightadd()
 {
     var navbarHeigh = $('#main-menu').height();
     var wrapperHeigh = $('#collapsibleNavbar').height();
-    var heightadd = wrapperHeigh + 150;
-
+    var heightadd = wrapperHeigh + 100;
+    alert(wrapperHeigh);
     if (wrapperHeigh > navbarHeigh) {
         $('#main-menu').css("min-height", heightadd + "px");
-
+        alert(heightadd);
     }
     if (navbarHeigh < wrapperHeigh) {
         $('#main-menu').css("min-height", $(window).height() + "px");
