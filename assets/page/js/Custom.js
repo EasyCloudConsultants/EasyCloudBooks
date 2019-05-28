d1b277964831f4a45e49cb42934b1aafbd479dc8
Dharmendra Patel <46588108+dharmendraecb@users.noreply.github.com>
2019-05-28 17:32:23 +0530

    Update Custom.js

--- assets/page/js/Custom.js
+++ assets/page/js/Custom.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    var description = location.pathname.toLowerCase();
+    var link = location.pathname.toLowerCase();
     var part = link.split("#");
 
     var titel = document.createElement('meta');
