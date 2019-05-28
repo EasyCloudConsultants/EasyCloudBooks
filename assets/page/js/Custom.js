f59e9b4ef66c198f8b722a30ffa2f1e65a703c71
Dharmendra Patel <46588108+dharmendraecb@users.noreply.github.com>
2019-05-28 17:29:13 +0530

    Update Custom.js

--- assets/page/js/Custom.js
+++ assets/page/js/Custom.js
@@ -1,9 +1,13 @@
 $(document).ready(function () {
-    var meta = location.pathname.toLowerCase();
+    var description = location.pathname.toLowerCase();
     var part = link.split("#");
 
-    var meta = document.createElement('meta');
-    meta.setAttribute('name', 'titel');
+    var titel = document.createElement('meta');
+    titel.setAttribute('name', 'titel');
+    var description = document.createElement('meta');
+    description.setAttribute('name', 'description');
+    var image = document.createElement('meta');
+    image.setAttribute('name', 'image');
   
 
     if (part[0] != "")
@@ -12,13 +16,15 @@ $(document).ready(function () {
         if (part[0] == "/user/company") {
 
             meta.content = "How to manage on EasyCloudBooks?";
-           
-
+            description.content = "This module enables you to access all your companies information within seconds. Apart from general information like company’s name, address, industry type, legal structure, it also provides essential details such as pending projects, projects on hold, billed amount, balance pending, custom details pending if unavailable, and its manager/contact person who deals with the matter. It also informs about the relationship manager i.e., any member of your staff whom the projects of that company have been assigned. "
+            image.content = "faqimages/company/acompany.png";
         }
 
     }
 
     document.getElementsByTagName('head')[3].appendChild(meta);
+    document.getElementsByTagName('head')[4].appendChild(description);
+    document.getElementsByTagName('head')[5].appendChild(image);
 
     var url = location.href;
     $('.main-menu li').find('a[href="' + url + '"]').parents('li').addClass('active');
