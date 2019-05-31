f215f729ab5863d7acbc738756a5b0c6355e47b9
Dharmendra Patel <46588108+dharmendraecb@users.noreply.github.com>
2019-05-31 11:25:27 +0530

    update 10

--- assets/page/js/Custom.js
+++ assets/page/js/Custom.js
@@ -1,29 +1,5 @@
 $(document).ready(function () {
-    var link = location.pathname.toLowerCase();
-    var part = link.split("#");
-
-
-    var titel = "";
-    var description = "";
-    var image = "";
-    var Urllink = "";
- //  var titleobj = $('meta[property="og:title"]');
-    if (part[0] != "")
-    {
-
-       
-  //  console.log(titleobj);
-    //console.log(titel);
-   // if(titleobj)    $(titleobj).attr('content',titel);
-    }
-   // $("head").append(Urllink);
-   // $("head").append(image);
-   // $("head").append(description);
- 
-  //  document.getElementsByTagName('head')[3].appendChild(titel);
-    //document.getElementsByTagName('head')[4].appendChild(description);
-    //document.getElementsByTagName('head')[5].appendChild(image);
-
+  
     var url = location.href;
     $('.main-menu li').find('a[href="' + url + '"]').parents('li').addClass('active');
 
@@ -53,7 +29,7 @@ $(document).ready(function () {
     }
 
     function heightadd() {
-        
+        var mainmenu = $('.navbarfull1').height();      
         var navbarHeigh = $('#main-menu').height();
         var wrapperHeigh = $('#collapsibleNavbar').height();
 		var newheight = $('.bd-content').height();
@@ -62,13 +38,13 @@ $(document).ready(function () {
         if (heightadd > navbarHeigh) {
             $('#main-menu').css("min-height", heightadd + "px");
 			//$('.navbarfull1').css("min-height", newheight +  "px");
-			//$('.navbarfull2').css("min-height", newheight + 32 +  "px");
+			$('.navbarfull2').css("min-height", mainmenu + 32 +  "px");
 
         }
         if (navbarHeigh > heightadd) {
             $('#main-menu').css("min-height", heightadd + "px");
-			//$('.navbarfull1').css("min-height", newheight + "px");
-			//$('.navbarfull2').css("min-height", newheight + 32 + "px");
+			//$('.navbarfull1').css("min-height", mainmenu + "px");
+			$('.navbarfull2').css("min-height", mainmenu + 32 + "px");
             
         }
     }
