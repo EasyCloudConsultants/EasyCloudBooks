ad880b4f55b29d329ff4e6937e5c8635b0434aec
Avinash Koshti <46588125+Avinash-ecb@users.noreply.github.com>
2019-07-17 14:26:45 +0530

    Create magnify.js

---
+++ assets/plugins/jquery.magify/js/magnify.js
@@ -0,0 +1,19 @@
+ $('[data-magnify]').magnify({
+    headToolbar: [
+      'minimize',
+      'maximize',
+      'close'
+    ],
+    footToolbar: [
+      'prev',
+      'next',
+      'zoomIn',
+      'zoomOut',
+      'fullscreen',
+      'actualSize',
+      'rotateLeft',
+      'rotateRight'
+    ],
+    modalWidth: 400,
+    modalHeight: 400
+  });
