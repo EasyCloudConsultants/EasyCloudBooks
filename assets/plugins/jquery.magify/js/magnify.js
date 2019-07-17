a83aba0208839afcc9a030298c458a3b363f4b00
Avinash Koshti <46588125+Avinash-ecb@users.noreply.github.com>
2019-07-17 15:24:10 +0530

    Update magnify.js

--- assets/plugins/jquery.magify/js/magnify.js
+++ assets/plugins/jquery.magify/js/magnify.js
@@ -1,8 +1,4 @@
-	$(".imgmargin").click(function(){
-debugger;
-		$(this).parent('a').attr("data-magnify","gallery").attr("data-caption", $(this).parent('a').attr("title"));
-		$(this).parent('a').removeClass( "fancybox-buttons" )
-	}); 
+
  
 
 $('[data-magnify]').magnify({
